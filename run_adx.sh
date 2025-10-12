352f9a9991c7dcaa4a5ace3e7be98088002fa4b7
anandarajRepo <anandaraj76@gmail.com>
2025-10-12 19:41:58 +0530

    batch file updated

--- run_adx.sh
+++ run_adx.sh
@@ -3,4 +3,4 @@ truncate -s 0 /var/log/adx.log
 truncate -s 0 cd /root/FyersADX/adx.log
 cd /root/FyersADX
 source venv/bin/activate
-python -u main.py run 2>&1 | tee -a adx.log
+python -u main.py run --paper 2>&1 | tee -a adx.log
