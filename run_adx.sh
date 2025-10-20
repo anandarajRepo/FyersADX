b338a2617ed88b399b50c91b93b4509775dc1fee
anandarajRepo <anandaraj76@gmail.com>
2025-10-20 14:54:54 +0530

    Updated python version in batch file

--- run_adx.sh
+++ run_adx.sh
@@ -3,4 +3,4 @@ truncate -s 0 /var/log/adx.log
 truncate -s 0 cd /root/FyersADX/adx.log
 cd /root/FyersADX
 source venv/bin/activate
-python -u main.py run --paper 2>&1 | tee -a adx.log
+python3.11 -u main.py run --paper 2>&1 | tee -a adx.log
